94268453963ecc056ed9644110de6fa2538d6d97
Henriquepython <joaohbp59@gmail.com>
2022-05-16 11:44:35 -0300

    feat: form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,7 @@ import { LiveFormDialogComponent } from './views/home/live-form-dialog/live-form
 import { MatNativeDateModule } from '@angular/material/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
@@ -45,7 +46,8 @@ import {MatInputModule} from '@angular/material/input';
     MatInputModule,
     MatNativeDateModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatFormFieldModule
   ],
   providers: [
   LocalDateTimePipe
--- src/app/views/home/live-form-dialog/live-form-dialog.component.ts
+++ src/app/views/home/live-form-dialog/live-form-dialog.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
+import { LiveService } from 'src/app/shared/service/live.service';
 
 @Component({
   selector: 'app-live-form-dialog',
@@ -11,6 +12,7 @@ export class LiveFormDialogComponent implements OnInit {
   public liveForm: FormGroup;
 
   constructor(
+    private rest: LiveService,
     private fb: FormBuilder,
     public dialogRef: MatDialogRef<LiveFormDialogComponent>
   ) { }
@@ -20,13 +22,21 @@ export class LiveFormDialogComponent implements OnInit {
       liveName: ['', [Validators.required]],
       channelName: ['', [Validators.required]],
       liveLink: ['', [Validators.required]],
-      liveDate: ['', [Validators.required]],
+      liveDate: ['2020-08-01T20:00:00', [Validators.required]],
       liveTime: ['', [Validators.required]]
     });
   }
 
+  createLive(): void {
+    this.rest.postLives(this.liveForm.value).subscribe(result => {});
+    this.dialogRef.close();
+    this.liveForm.reset();
+  }
+
   cancel(): void {
     this.dialogRef.close();
+    this.liveForm.reset();
   }
+
 
 }
